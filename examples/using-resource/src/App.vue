55c0b5a9fd12bb40a43eae6bd670da7e2e9b2f28
Max Gfeller <mg@cyon.ch>
2020-06-08 12:19:15 +0200

    Demo refresh and fetch

--- examples/using-resource/src/App.vue
+++ examples/using-resource/src/App.vue
@@ -10,46 +10,57 @@
     <br><br>
     Count: {{ count }}<br>
     <button @click="count++">Increase Count</button>
+    <br><br>
+    <button @click="refreshResource">Refresh Resource</button>
+    <br><br>
+    <button @click="fetchUsers">Fetch users</button>
   </div>
 </template>
 
 <script>
 import { ref, reactive } from 'vue'
-import { usePagination } from '../../../'
+import { usePagination, resource } from '../../../'
 
 export default {
   name: 'App',
   setup () {
-    // const users = usePagination('users', { page: 1, pageSize: 2 })
-
     const count = ref(1)
+    const users = usePagination('users', { page: 1, pageSize: 2, args: reactive({ count }) })
 
-    const usersArray = [
-      'a',
-      'b',
-      'c',
-      'd',
-      'e',
-      'f',
-      'g',
-      'h'
-    ]
-    const users = usePagination(async (opts) => {
-      const offset = opts.page * opts.pageSize - opts.pageSize
+    // const usersArray = [
+    //   'a',
+    //   'b',
+    //   'c',
+    //   'd',
+    //   'e',
+    //   'f',
+    //   'g',
+    //   'h'
+    // ]
+    // const users = usePagination(async (opts) => {
+    //   const offset = opts.page * opts.pageSize - opts.pageSize
 
-      return {
-        total: usersArray.length,
-        items: usersArray.slice(offset, offset + opts.pageSize).map(user => user + opts.args.count.value)
-      }
-    }, {
-      page: 1,
-      pageSize: 2,
-      args: reactive({
-        count
-      })
-    })
+    //   return {
+    //     total: usersArray.length,
+    //     items: usersArray.slice(offset, offset + opts.pageSize).map(user => user + opts.args.count.value)
+    //   }
+    // }, {
+    //   page: 1,
+    //   pageSize: 2,
+    //   args: reactive({
+    //     count
+    //   })
+    // })
+    const fetchUsers = async () => {
+      const users = await resource('users').fetchRange({ args: { count: count.value } })
+      console.log('users', users)
+    }
 
-    return { users, count }
+    const refreshResource = () => {
+      resource('users').refresh()
+    }
+
+    return { users, count, refreshResource, fetchUsers }
   }
 }
 </script>
